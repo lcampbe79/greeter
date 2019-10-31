801e65b85da923600206cf38524366a693399f4c
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-10-31 10:48:56 -0700

    responds with MSG.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
-
+console.log(`\n message:`, process.env.MSG)
 const server = express(); // creating an instance of an express server
 
 server.get('/', (req, res) => {
-  res.status(200).json({ hello: 'Web 23' });
+  res.status(200).json({ message: process.env.MSG });
 });
 
 const port = process.env.PORT || 4000;
